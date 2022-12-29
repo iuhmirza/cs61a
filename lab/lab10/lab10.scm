9237df03f6e9824fd7a252e005250cbb1754915d
Ibtisam Mirza <iuhmirza@gmail.com>
2022-12-29 12:38:01 +0000

    Complete lab10

---
+++ lab/lab10/lab10.scm
@@ -0,0 +1,24 @@
+(define (over-or-under num1 num2)
+  (cond 
+    ((= num1 num2) 0)
+    ((< num1 num2) -1)
+    (else 1)))
+
+(define (make-adder num)
+  (define (adder x) (+ num x))
+  adder)
+
+(define (composed f g)
+    (lambda (x) (f (g x))))
+
+(define lst 
+    (cons
+        (cons 1 nil)
+        (cons 2 
+              (cons (cons 3 (cons 4 nil))
+                    (cons 5 nil)))))
+
+(define (duplicate lst)
+    (if (null? lst)
+        ()
+        (cons (car lst) (cons (car lst) (duplicate (cdr lst))))))
